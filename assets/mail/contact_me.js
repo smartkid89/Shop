5c53b138bb85745d76bbec5d1733c564091a3d6c
smartkid89 <69963410+smartkid89@users.noreply.github.com>
2020-09-22 01:04:39 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -66,7 +66,7 @@ $(function () {
                 },
                 complete: function () {
                     setTimeout(function () {
-                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                        $this.prop("enabled", false); // Re-enable submit button when AJAX call is complete
                     }, 1000);
                 },
             });
